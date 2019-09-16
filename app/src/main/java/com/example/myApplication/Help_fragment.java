089ba112432f370ff0b5350f795cbb3355e3a19b
Sarita-Joshi <joshisarita1311@gmail.com>
2019-09-16 09:35:29 +0530

    added bot nav

---
+++ app/src/main/java/com/example/myApplication/Help_fragment.java
@@ -0,0 +1,26 @@
+package com.example.myApplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+
+public class Help_fragment extends Fragment {
+
+
+    Help_fragment()
+    {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_help_fragment, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myApplication/Profile_fragment.java
@@ -0,0 +1,25 @@
+package com.example.myApplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class Profile_fragment extends Fragment {
+
+
+    Profile_fragment()
+    {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_fragment, container, false);
+    }
+
+
+}
